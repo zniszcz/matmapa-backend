213c1037dbe0126fe06dd046ed7685032a6dbc1a
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2017-02-11 13:41:01 +0100

    Typo fix

--- .docker/build.sh
+++ .docker/build.sh
@@ -6,4 +6,4 @@ PROJECT_DIR=`pwd`
 
 cp $TMP_COMPOSE $COMPOSE
 sed -i "s|<<<project_dir>>>|$PROJECT_DIR|g" "$COMPOSE"
-docker build -t matmapa-npm . # ".docker/docker_node_app"
+docker build -t matmapa-nvm . # ".docker/docker_node_app"
