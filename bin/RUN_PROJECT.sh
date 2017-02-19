0f89f42b1089d113773e13fc5597b8b8e8fd932c
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2017-02-19 20:09:14 +0100

    Change project directory structure

--- bin/RUN_PROJECT.sh
+++ bin/RUN_PROJECT.sh
@@ -1,7 +1,10 @@
-if [ $1 = "--env=production" ]
-  then
-    echo "Run in prodction mode"
-    export NODE_ENV=production
+if [ ! -z "$1" ]
+    then
+        if [ $1 = "--env=production" ]
+          then
+            echo "Run in prodction mode"
+            export NODE_ENV=production
+    fi
 fi
 
-pm2 start /matmapa/src/app.js --name="matmapa-backend"
+pm2 start /matmapa/server.js --name="matmapa-backend"
