1f194a242be734e8b3b3b2b95db960daaa4da54d
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2017-02-20 00:23:27 +0100

    Add hash in authentication

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const routes = require('express').Router();
 const config = require('../utils/config');
+const crypto = require('crypto');
 
 module.exports = (req, res) => {
   if (!req.body.name || !req.body.password) {
@@ -17,10 +18,21 @@ module.exports = (req, res) => {
     if (err) throw err;
 
     if (!user) {
-      res.json({ success: false, message: 'Authentication failed. User not found'});
+      res.json({
+        success: false,
+        message: 'Authentication failed. User not found'
+      });
     } else {
-      if (user.password != req.body.password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.'});
+      const userPassword = crypto
+        .createHash('sha256')
+        .update(req.body.password)
+        .digest('hex');
+
+      if (req.body.password != userPassword) {
+        res.json({
+          success: false,
+          message: 'Authentication failed. Wrong password.'
+        });
       } else {
         const token = jwt.sign(user, config.secret, {
           expiresIn: 60*60*24
