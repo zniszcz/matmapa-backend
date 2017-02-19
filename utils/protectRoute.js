ca579fc7c9a27844a3e5f41485c870bd4cb7fd56
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2017-02-19 23:28:20 +0100

    Remove unnecessary token sources

--- utils/protectRoute.js
+++ utils/protectRoute.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const config = require('./config');
 
 module.exports = (req, res, next) => {
-  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  const token = req.headers['x-access-token'];
   if (token) {
     jwt.verify(token, config.secret, (err, decoded) => {
       if (err) {
