098b8b0ce57ddbe9329de7f32d5f0b89e8d5d4a4
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2017-10-11 23:41:25 +0200

    Add default models

--- models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-module.exports = mongoose.model('User', new Schema({
-  name: String,
-  email: String,
-  password: String,
-}));
